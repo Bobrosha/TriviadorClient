e356c4bc9e4ee82cfeb320a4bf40f38e32801e6a
SHREDER <adutov@icloud.com>
2022-01-10 21:24:27 +0700

    upd

--- Questions.xaml.cs
+++ Questions.xaml.cs
@@ -1,37 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using TriviadorClient.Entities;
 
 namespace TriviadorClient
 {
-   /// <summary>
-   /// Логика взаимодействия для Questions.xaml
-   /// </summary>
-   public partial class Questions : Window
-   {
-      private readonly Client _Client;
-      public Questions(Client client)
-      {
-         _Client = client;
-         InitializeComponent();
-      }
+    /// <summary>
+    /// Логика взаимодействия для Questions.xaml
+    /// </summary>
+    public partial class Questions : Window
+    {
+        private readonly Client _Client;
+        private DispatcherTimer _Timer;
+        private int _DeadTimer;
+        public Questions(Client client)
+        {
+            _Client = client;
+            InitializeComponent();
+            _Timer = new DispatcherTimer();
+            _Timer.Interval = TimeSpan.FromMilliseconds(1000);
+            _Timer.Tick += Timer_Tick;
+            _Timer.Start();
+            _DeadTimer = 60;
+        }
 
-      private void Button_Click_EndQuestion(object sender, EventArgs e)
-      {
-         System.Threading.Thread.Sleep(5000);
-         WindowQuestions.Close();
-      }
-   }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _DeadTimer -= (int)_Timer.Interval.TotalSeconds;
+            TextBlockTimer.Text = _DeadTimer.ToString();
+            if (_DeadTimer <= 0)
+            {
+                _Timer.Stop();
+                WindowQuestions.Close();
+            }
+        }
+
+        private void Button_Click_EndQuestion(object sender, EventArgs e)
+        {
+            WindowQuestions.Close();
+        }
+    }
 }
